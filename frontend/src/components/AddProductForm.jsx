6be702a8370969233a7a3d54c22993e1fa2977b5
orhanors <orsorhan1@gmail.com>
2020-12-11 16:58:10 +0300

    delete and add product form ok

--- frontend/src/components/AddProductForm.jsx
+++ frontend/src/components/AddProductForm.jsx
@@ -1,45 +1,62 @@
 import React from "react";
 import { Form, Button, Container } from "react-bootstrap";
+//
 const AddProductForm = (props) => {
 	return (
 		<div className='product-form'>
 			<Container>
-				<Form>
-					<Form.Group controlId='exampleForm.ControlInput1'>
+				<Form onSubmit={props.onHandleSubmit}>
+					<Form.Group>
 						<Form.Label>Name</Form.Label>
 						<Form.Control
 							id='name'
 							type='text'
-							placeholder='name@example.com'
+							placeholder='Name'
+							value={props.product.name}
+							onChange={props.fillForm}
 						/>
 					</Form.Group>
 
-					<Form.Group controlId='exampleForm.ControlTextarea1'>
+					<Form.Group>
 						<Form.Label>Description</Form.Label>
-						<Form.Control id='description' as='textarea' rows={2} />
+						<Form.Control
+							id='description'
+							as='textarea'
+							rows={2}
+							value={props.product.description}
+							onChange={props.fillForm}
+						/>
 					</Form.Group>
 
-					<Form.Group controlId='exampleForm.ControlInput1'>
+					<Form.Group>
 						<Form.Label>Brand</Form.Label>
 						<Form.Control
 							id='brand'
 							type='text'
-							placeholder='name@example.com'
+							placeholder='Brand Name'
+							value={props.product.brand}
+							onChange={props.fillForm}
 						/>
 					</Form.Group>
 
-					<Form.Group controlId='exampleForm.ControlInput1'>
+					<Form.Group>
 						<Form.Label>Price</Form.Label>
 						<Form.Control
 							id='price'
 							type='number'
 							placeholder='name@example.com'
+							value={props.product.price}
+							onChange={props.fillForm}
 						/>
 					</Form.Group>
 
-					<Form.Group controlId='exampleForm.ControlSelect1'>
+					<Form.Group>
 						<Form.Label>Category</Form.Label>
-						<Form.Control as='select'>
+						<Form.Control
+							id='category'
+							as='select'
+							value={props.product.category}
+							onChange={props.fillForm}>
 							<option>Tech</option>
 							<option>Home </option>
 							<option>Food</option>
--- frontend/src/components/BackOffice.jsx
+++ frontend/src/components/BackOffice.jsx
@@ -1,77 +1,134 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { Button, Image, Spinner, Table } from "react-bootstrap";
+import { Button, Container, Image, Spinner, Table } from "react-bootstrap";
 import { getProducts } from "../api/productsApi";
 import AddProductForm from "./AddProductForm";
+import { postProduct, removeProduct } from "../api/productsApi";
 const BackOffice = (props) => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+	const [products, setProducts] = useState([]);
+	const [product, setProduct] = useState({
+		name: "",
+		description: "",
+		brand: "",
+		price: 1,
+		category: "Tech",
+	});
 
-  const fetchProducts = async () => {
-    setLoading(true);
-    const products = await getProducts();
-    setProducts(products);
-    setLoading(false);
-  };
+	const [submittedSize, setSubmittedSize] = useState(0);
 
-  return (
-    <div className="back-office">
-      <h1 className="back-office-title text-center my-3">HOMEROS SHOP</h1>
-      <h3 className="back-office-subtitle text-center mb-2">Back Office</h3>
+	const [loading, setLoading] = useState(false);
+	useEffect(() => {
+		const mileStone = async () => {
+			await fetchProducts();
+		};
 
-      <AddProductForm />
+		mileStone();
+	}, [submittedSize]);
 
-      <div className="product-table">
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Brand</th>
-              <th>Price</th>
-              <th>Category</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {loading ? (
-              <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-              </Spinner>
-            ) : (
-              <>
-                {products &&
-                  products.map((product, index) => {
-                    return (
-                      <tr>
-                        <td>{index + 1}</td>
-                        <td>
-                          <Image src={product.imageUrl} />
-                        </td>
-                        <td>{product.name}</td>
-                        <td>{product.description}</td>
-                        <td>{product.brand}</td>
-                        <td>{product.price}</td>
-                        <td>{product.category}</td>
-                        <td>
-                          <Button>Update</Button>
-                          <Button>Remove</Button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </>
-            )}
-          </tbody>
-        </Table>
-      </div>
-    </div>
-  );
+	const fetchProducts = async () => {
+		setLoading(true);
+		const products = await getProducts();
+		setProducts(products);
+		setLoading(false);
+	};
+
+	const addProduct = async (e) => {
+		e.preventDefault();
+		let postedProduct = await postProduct(product);
+		setSubmittedSize(submittedSize + 1);
+		console.log(postedProduct);
+	};
+	const fillForm = (e) => {
+		let currentId = e.currentTarget.id;
+		let newProduct = { ...product };
+		newProduct[currentId] = e.currentTarget.value;
+		setProduct(newProduct);
+	};
+
+	const deleteProduct = async (e) => {
+		let id = e.target.id;
+		console.log(e.currentTarget);
+		let deletedResponse = await removeProduct(id);
+		setSubmittedSize(submittedSize + 1);
+	};
+	return (
+		<div className='back-office'>
+			<h1 className='back-office-title text-center my-3'>HOMEROS SHOP</h1>
+			<h3 className='back-office-subtitle text-center mb-2'>
+				Back Office
+			</h3>
+
+			<AddProductForm
+				product={product}
+				fillForm={fillForm}
+				onHandleSubmit={addProduct}
+			/>
+
+			<div className='product-table mt-3'>
+				<Container>
+					<Table striped bordered hover variant='dark'>
+						<thead>
+							<tr>
+								<th>No</th>
+								<th>Image</th>
+								<th>Name</th>
+								<th>Description</th>
+								<th>Brand</th>
+								<th>Price</th>
+								<th>Category</th>
+								<th>Actions</th>
+							</tr>
+						</thead>
+						<tbody>
+							{loading ? (
+								<Spinner animation='border' role='status'>
+									<span className='sr-only'>Loading...</span>
+								</Spinner>
+							) : (
+								<>
+									{products &&
+										products.map((product, index) => {
+											return (
+												<tr>
+													<td>{index + 1}</td>
+													<td>
+														<Image
+															src={
+																product.imageUrl
+															}
+														/>
+													</td>
+													<td>{product.name}</td>
+													<td>
+														{product.description}
+													</td>
+													<td>{product.brand}</td>
+													<td>{product.price}</td>
+													<td>{product.category}</td>
+													<td>
+														<Button className='mr-2'>
+															Update
+														</Button>
+														<Button
+															id={product._id}
+															onClick={(e) =>
+																deleteProduct(e)
+															}
+															variant='danger'>
+															Remove
+														</Button>
+													</td>
+												</tr>
+											);
+										})}
+								</>
+							)}
+						</tbody>
+					</Table>
+				</Container>
+			</div>
+		</div>
+	);
 };
 
 export default BackOffice;
