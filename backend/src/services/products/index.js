10d19474d027c5ed33c06a942d4a8d9f325d2609
Hung Jin Chong <hungjinchong@Outlook.com>
2020-12-11 10:08:01 +0000

    Added update and delete fucntion

--- backend/src/services/products/index.js
+++ backend/src/services/products/index.js
@@ -27,91 +27,133 @@ const reviewsFilePath = path.join(__dirname, "reviews.json");
  
  */
 const validateInput = (dataToValidate) => {
-	const schema = Joi.object().keys({
-		name: Joi.string().min(3).max(30).required(),
-		description: Joi.string().min(3).max(200).required(),
-		brand: Joi.string().min(3).required(),
-		price: Joi.number().required(),
-		category: Joi.string().min(2),
-	});
+  const schema = Joi.object().keys({
+    name: Joi.string().min(3).max(30).required(),
+    description: Joi.string().min(3).max(200).required(),
+    brand: Joi.string().min(3).required(),
+    price: Joi.number().required(),
+    category: Joi.string().min(2),
+  });
 
-	console.log(schema.validate(dataToValidate));
-	return schema.validate(dataToValidate); //error,value
+  console.log(schema.validate(dataToValidate));
+  return schema.validate(dataToValidate); //error,value
 };
 
 // "/" GET ALL PRODUCTS
 router.get("/", async (req, res, next) => {
-	try {
-		const productsDB = await readDB(productsFilePath);
-		if (req.query && req.query.category) {
-			const filteredData = productsDB.filter(
-				(product) =>
-					product.hasOwnProperty("category") &&
-					product.category
-						.toLowerCase()
-						.includes(req.query.category.toLowerCase())
-			);
-			res.send(filteredData);
-		}
-		res.send(productsDB);
-	} catch (error) {
-		console.log(error);
-		next(error);
-	}
+  try {
+    const allProducts = await readDB(productsFilePath);
+    if (req.query && req.query.category) {
+      const filteredData = allProducts.filter(
+        (product) =>
+          product.hasOwnProperty("category") &&
+          product.category
+            .toLowerCase()
+            .includes(req.query.category.toLowerCase())
+      );
+      res.send(filteredData);
+    }
+    res.send(allProducts);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 // "/" POST A PRODUCT
 router.post("/", async (req, res, next) => {
-	try {
-		const { error } = validateInput(req.body);
+  try {
+    const { error } = validateInput(req.body);
 
-		if (error) {
-			let err = new Error();
-			err.message = error.details[0].message;
-			err.httpStatusCode = 400;
-			next(err);
-		} else {
-			let newProduct = {
-				...req.body,
-				_id: uniqid(),
-				createdAt: new Date(),
-			};
+    if (error) {
+      let err = new Error();
+      err.message = error.details[0].message;
+      err.httpStatusCode = 400;
+      next(err);
+    } else {
+      let newProduct = {
+        ...req.body,
+        _id: uniqid(),
+        createdAt: new Date(),
+      };
 
-			const allProducts = await readDB(productsFilePath);
-			allProducts.push(newProduct);
+      const allProducts = await readDB(productsFilePath);
+      allProducts.push(newProduct);
 
-			await writeDB(productsFilePath, allProducts);
-			res.status(200).send(newProduct);
-		}
-	} catch (error) {
-		console.log(error);
-		next(error);
-	}
+      await writeDB(productsFilePath, allProducts);
+      res.status(200).send(newProduct);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 // "/:id" GET A SINGLE PRODUCT
 router.get("/:id", async (req, res, next) => {
-	try {
-		const allProducts = await readDB(productsFilePath);
-		let product = allProducts.find((p) => p._id === req.params.id);
-		if (!product) {
-			const err = new Error();
-			err.htttpStatusCode = 404;
-			next(err);
-		} else {
-			res.send(product);
-		}
-	} catch (err) {
-		console.log(err);
-		next(err);
-	}
+  try {
+    const allProducts = await readDB(productsFilePath);
+    let product = allProducts.find((p) => p._id === req.params.id);
+    if (!product) {
+      const err = new Error();
+      err.htttpStatusCode = 404;
+      next(err);
+    } else {
+      res.status(200).send(product);
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 // "/:id" UPDATE A PRODUCT
-router.put("/:id", async (req, res, next) => {});
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = validateInput(req.body);
+    const allProducts = await readDB(productsFilePath);
+    const newProducts = allProducts.filter(
+      (product) => product._id !== req.params.id
+    );
+    const modifiedProduct = req.body;
+    modifiedProduct._id = req.params.id;
+    modifiedProduct.updatedAt = new Date();
+    if (error) {
+      let err = new Error();
+      err.message = error.details[0].message;
+      err.httpStatusCode = 400;
+      next(err);
+    } else {
+      newProducts.push(modifiedProduct);
+      await writeDB(productsFilePath, newProducts);
+      res.status(200).send(modifiedProduct);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 // "/:id" REMOVE A PRODUCT
-router.delete("/:id", async (req, res, next) => {});
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const allProducts = await readDB(productsFilePath);
+    const newProducts = allProducts.filter(
+      (product) => product._id !== req.params.id
+    );
+    if (newProducts.length === allProducts.length) {
+      let error = new Error();
+      error.httpStatusCode = 404;
+      next(error);
+    } else {
+      await writeDB(productsFilePath, newProducts);
+      res.status(200).send(`Product with id:${req.params.id} deleted`);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 // "/:id/reviews" GET ALL REVIEWS FOR A SINGLE PRODUCT
 router.get("/:id/reviews", async (req, res, next) => {});
